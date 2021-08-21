ded9efffc3150e99efa9cb6c2c591fdcc10f5a6c
Tor Halle-McIntyre <torhallemcintyre@gmail.com>
2021-08-21 13:55:31 +1200

    Updated addon bl_info to reflect fork

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 bl_info = {
 	"name" : "Animation Retargeting",
-	"author" : "Mwni",
+	"author" : "Mwni, Mophs",
 	"description" : "Retarget animations from one rig to another",
-	"version": (1, 0, 0),
-	"blender" : (2, 80, 0),
+	"version": (1, 1, 0),
+	"blender" : (2, 90, 1),
 	"location" : "3D View > Tools (Right Side) > Retarget",
 	"warning" : "",
 	"category" : "Animation",
-	'wiki_url': 'https://github.com/Mwni/blender-animation-retargeting',
-    'tracker_url': 'https://github.com/Mwni/blender-animation-retargeting/issues',
+	'wiki_url': 'https://github.com/Mophs/blender-animation-retargeting',
+    'tracker_url': 'https://github.com/Mophs/blender-animation-retargeting',
 }
 
 from . import addon
